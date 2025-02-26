a9e9a760e39479fe3fed31d60d87e6ad29958361
John Lynson <johnlynson809@gmail.com>
2025-02-26 13:49:45 +0100

    Auth0 implementatie

---
+++ DeepBreaths.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[Route("api/auth")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpGet("userinfo")]
+    [Authorize]
+    public IActionResult GetUserInfo()
+    {
+        var user = User.Identity;
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+        return Ok(new
+        {
+            UserId = userId,
+            Email = email
+        });
+    }
+}
